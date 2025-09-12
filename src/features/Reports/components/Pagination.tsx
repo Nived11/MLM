d167bc6ded0e803d6fc63d625d930eb8bd29646e
Nived <nivedsuresh5@gmail.com>
2025-09-12 19:52:53 +0530

    initial commit

--- src/features/Reports/components/Pagination.tsx
+++ src/features/Reports/components/Pagination.tsx
@@ -2,11 +2,11 @@ const Pagination = ( ) => {
   return (
     <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center sm:items-center mt-6 text-sm text-gray-400 gap-3">
       <span className="text-xs sm:text-sm">Showing 0 to 0 of 365 entries</span>
-      <div className="flex gap-2 sm:gap-3 flex-wrap justify-center sm:justify-end">
-        <button className="hover:text-white text-xs sm:text-sm ">First</button>
-        <button className="hover:text-white text-xs sm:text-sm">Previous</button>
-        <button className="hover:text-white text-xs sm:text-sm">Next</button>
-        <button className="hover:text-white text-xs sm:text-sm">Last</button>
+      <div className="flex gap-8 sm:gap-6 flex-wrap justify-center sm:justify-end mt-5 items-center">
+        <button className="hover:text-white text-sm sm:text-sm ">First</button>
+        <button className="hover:text-white text-sm sm:text-sm">Previous</button>
+        <button className="hover:text-white text-sm sm:text-sm">Next</button>
+        <button className="hover:text-white text-sm sm:text-sm">Last</button>
       </div>
     </div>
   );
