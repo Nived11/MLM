13e5103c67ce2a32aa0354a06ad009fc3f4d8ae4
Nived <nivedsuresh5@gmail.com>
2025-09-16 01:40:53 +0530

    mb

--- src/pages/admin/UserManagement.tsx
+++ src/pages/admin/UserManagement.tsx
@@ -2,7 +2,7 @@ import {  SearchField,  UserCards} from "../../features/admin-UserManagement";
 
 const UserManagement = () => {
   return (
-    <div className="px-4 py-4 text-white mb-10 mt-0 lg:mt-20">
+    <div className="px-4 py-4 text-white mb-20 mt-0 lg:mt-20">
       <h1 className="text-lg sm:text-xl font-bold mb-4">User Management</h1>
 
       <SearchField />
