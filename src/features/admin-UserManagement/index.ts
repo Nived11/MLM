4d25ce508341b6df661e408f035a6aba71dd10f0
Nived <nivedsuresh5@gmail.com>
2025-09-16 01:38:49 +0530

    admin

--- src/features/admin-UserManagement/index.ts
+++ src/features/admin-UserManagement/index.ts
@@ -1,3 +1,2 @@
 export { default as SearchField } from "./components/SearchField"
-export { default as UserCards} from "./components/UserCards" 
-export { default as SkeletonCard } from "./components/UserCardSkeleton"
+export { default as UserCards} from "./components/UserCards"
